2d51d08630f213b119d3fd3ade7a4f8385888232
Sandra Houston <sandrahouston@sandras-air>
2018-06-28 13:48:24 +0100

    Initial Commit

---
+++ Higher Maths Revision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Higher Maths Revision
+//
+//  Created by Sandra Houston on 28/06/2018.
+//  Copyright © 2018 sandshouston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
